5a02837f5b5bae3324e801999c6ac3325fde4606
Priyeshkumar786 <mishrapriyesh.521@gmail.com>
2025-12-24 16:34:57 +0530

    okk

---
+++ src/pages/Doctors.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const Doctors = () => {
+
+    const { speciality } = useParams()
+    console.log(speciality);
+    
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Doctors
